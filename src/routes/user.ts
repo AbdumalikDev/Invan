bd631c4e39fc3e149d2b5d61284542a4cd343e53
Abdumalik Khojimirzaev <hojimirzaevabdumalik@gmail.com>
2021-06-16 16:41:16 +0500

    gitignore added

--- src/routes/user.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { UserController } from '../controllers/user'
-
-const router = Router({ mergeParams: true })
-const controller = new UserController()
-
-router.route('/register').post(controller.register)
-
-export default router
