f0507615240287ed6c1904aee1bd666648ea768b
Samandar Obidov <samandarobidovfrd@gmail.com>
2021-07-03 21:25:22 +0500

    added

--- src/config/config.ts
+++ src/config/config.ts
@@ -15,7 +15,7 @@ interface Config {
 }
 
 let config: Config = {
-    HttpPort: getConf('HTTP_PORT', ''),
+    HttpPort: getConf('PORT', ''),
     MongoHost: getConf('MONGO_HOST', 'localhost'),
     MongoPort: parseInt(getConf('MONGO_PORT', '27017')),
     MongoDatabase: getConf('MONGO_DATABASE', 'invan_myproject'),
--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ import { logger } from './config/logger'
         const db = new DB()
         db.connect()
 
-        app.listen(config.HttpPort || 8080, () => {
-            logger.info(`INDEX: Server is running on port: ${config.HttpPort || 8080}`)
+        app.listen(config.HttpPort, () => {
+            logger.info(`INDEX: Server is running on port: ${config.HttpPort}`)
         })
 
         logger.info('INDEX: Database connection initialized.')
