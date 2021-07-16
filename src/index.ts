a10533dc90c3895b3b92369e77dd03668a646af0
Samandar Obidov <samandarobidovfrd@gmail.com>
2021-07-16 15:26:26 +0500

    added for pulling;

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ import { logger } from './config/logger'
         const db = new DB()
         db.connect()
 
-        app.listen(config.HttpPort, () => {
-            logger.info(`INDEX: Server is running on port: ${config.HttpPort}`)
+        app.listen(config.HttpPort || 3005, () => {
+            logger.info(`INDEX: Server is running on port: ${config.HttpPort || 3005}`)
         })
 
         logger.info('INDEX: Database connection initialized.')
